b5f5176295d790fbc69a1a2ec49204b048e15f1c
wanjanotfound <wwanjiku727@gmail.com>
2024-05-11 02:19:17 +0300

    updates

---
+++ index.js
@@ -0,0 +1,7 @@
+let signUp = document.getElementById("signup")
+let signOut = document.getElementById("signout")
+let logIn = document.getElementById("login")
+
+signUp.addEventListener("click", signingup() {
+
+})

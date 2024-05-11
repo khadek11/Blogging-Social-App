c69f8fd2c8991b002e13f1b5b851300f58073b3d
mercycoding <mk7693964@gmail.com>
2024-05-11 19:02:15 +0300

    final commit

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const dropdownBtn = document.getElementById("btn-menu");
+const dropdownMenu = document.getElementById("dropdown");
+const toggleArrow = document.getElementById("arrow-menu");
+
+const toggleDropdown = function () {
+    dropdownMenu.classList.toggle("show");
+    toggleArrow.classList.toggle("arrow");
+  };
+  dropdownBtn.addEventListener("click", function (e) {
+    e.stopPropagation();
+    toggleDropdown();
+  });
+
+  document.documentElement.addEventListener("click", function () {
+    if (dropdownMenu.classList.contains("show")) {
+      toggleDropdown();
+    }
+  });
 
 /*sign up*/
 var signUp = document.getElementById("signup")

567fd9642d13375685e457c96f7c5fa9b8ef003b
jkibura <167850144+jkibura@users.noreply.github.com>
2024-05-11 05:54:24 +0300

    Jjj blogpage (#13)
    * added blog page
    
    * lalasalama changes
    
    * last last changes

--- index.js
+++ index.js
@@ -38,3 +38,27 @@ signUp.addEventListener("click", signingup() {
 
 })
 
+
+//js for blog posts
+var likesCount = 0;
+
+function addLike() {
+  likesCount++;
+  document.getElementById('likesCount').textContent = likesCount + ' Likes';
+}
+
+function addComment() {
+  var commentInput = document.getElementById('commentInput');
+  var commentText = commentInput.value.trim();
+
+  if (commentText !== '') {
+    var commentsDiv = document.getElementById('comments');
+    var newComment = document.createElement('p');
+    newComment.textContent = commentText;
+    commentsDiv.appendChild(newComment);
+
+    
+    commentInput.value = '';
+  }
+}
+

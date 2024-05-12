60615be6d2a223124c3337d24f121a6e52ed7c0b
wanjanotfound <155843876+wanjanotfound@users.noreply.github.com>
2024-05-12 14:27:26 +0300

    Update index.js
    updated email and password alerts

--- index.js
+++ index.js
@@ -31,11 +31,12 @@ signUp.addEventListener("click", function() {
         var email = document.getElementById("signup-email").value;
         
         var password = document.getElementById("signup-password").value;
-                    alert("please fill in all fields.")
+                    if(!email || !password) {
+                        alert("please fill in all fields.");
             return;
         }
 
-    );
+    });
 
 
     /* login in*/
@@ -50,11 +51,13 @@ signUp.addEventListener("click", function() {
             var email = document.getElementById("login-email").value;
             
             var password = document.getElementById("login-password").value;
-                        alert("please fill in all fields.")
-                return;
+            if(!email || !password) {
+                alert("please fill in all fields.");
+    return;
+        
             }
     
-        );
+        });
     
     // Search functionality
 document.getElementById("search-btn").addEventListener("click", function() {
